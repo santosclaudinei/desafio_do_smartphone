9c28b1afee0e9963095ca6706be409361a72b346
Claudinei Santos <claudinei.santos@warren.com.br>
2022-12-21 17:48:40 -0300

    feat: ajustando classe smartphone e criando logicas na program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,18 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+var iphoneDoJose = new Iphone("11999998888", "Iphone X", "111111111111", 8);
+var nokiaDaMaria = new Nokia("21988886666", "Nokia X", "222222222222", 16);
+var nomeDoApp = "Whatsapp";
+    
+iphoneDoJose.Ligar();
+iphoneDoJose.ReceberLigacao();
+iphoneDoJose.InstalarAplicativo(nomeDoApp);
+iphoneDoJose.MostraConfiguracao(iphoneDoJose.Modelo, iphoneDoJose.Memoria);
+
+Console.WriteLine("\n");
+
+nokiaDaMaria.Ligar();
+nokiaDaMaria.ReceberLigacao();
+nokiaDaMaria.InstalarAplicativo(nomeDoApp);
+nokiaDaMaria.MostraConfiguracao(nokiaDaMaria.Modelo, nokiaDaMaria.Memoria);
